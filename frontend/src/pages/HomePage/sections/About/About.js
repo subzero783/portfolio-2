1e25c4d3018cb05de06bbef5753edc29e5116c4c
Gustavo Amezcua <gamezcua@ascentfunding.com>
2022-09-05 20:58:00 -0700

    Working on About section

--- frontend/src/pages/HomePage/sections/About/About.js
+++ frontend/src/pages/HomePage/sections/About/About.js
@@ -23,6 +23,13 @@ export default function About(){
                         </div>
                     </div>
                     <div className="col col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+                        <div id="about-me-text">
+                            <h2>About Me</h2>
+                            <div className="text-underline-1"></div>
+                            <p className="text-1">
+                            Full-stack web developer with over 5 years of experience in producing pixel-perfect websites and solutions with advanced knowledge in coding. Eager to support development teams with top-notch coding skills. In previous roles, was able to adopt new CMS platforms such as HubSpot and also new coding technologies such as NodeJS, ReactJS and API Integrations.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
