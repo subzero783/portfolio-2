952259b8ba2cf79b5a3a35f32d67bc27f084fea1
Gustavo <gusta1296@gmail.com>
2022-11-02 21:28:24 -0700

    Need to show youtube videos from my channel

--- frontend/src/pages/HomePage/sections/YouTubeVideos/YouTubeVideos.js
+++ frontend/src/pages/HomePage/sections/YouTubeVideos/YouTubeVideos.js
@@ -1,5 +1,5 @@
 
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import './YouTubeVideos.scss';
@@ -19,7 +19,11 @@ const page_scroll_2 = () => {
     }
 }
 
+
 export default function YouTubeVideos(){
+
+    const [videos, setVideos] = useState([]);
+    // const youtubeURL = `https://www.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_GOOGLE_API}&channelId=${process.env.REACT_APP_YOUTUBE_CHANNEL_ID}&part=snippet,id&order=date&maxResults=20`;
 
     window.addEventListener('scroll', page_scroll_2);
 
@@ -30,13 +34,21 @@ export default function YouTubeVideos(){
         });
     }, []);
 
+    useEffect(()=>{
+        // const fetchVideos = async(url) => {
+        //     const result = await fetch(url);
+        //     const body = await result.json();
+        //     console.log(body);
+        // }
+        // fetchVideos(youtubeURL);
+    }, []);
+
     return(
         <div id="youtube-videos">
             <div className="container">
                 <div className="row">
                     <div id="youtube-videos-text" className="title-1">
                         <h2>
-                            
                             <div>M</div>
                             <div>y</div>
                             <div className="empty_text"></div>
@@ -57,12 +69,13 @@ export default function YouTubeVideos(){
                             <div>l</div>
                         </h2>
                     </div>
-                    <h3 class="subtitle">Watch my latest videos</h3>
+                    <h3 className="subtitle">Watch my latest videos</h3>
                     {/* Add video player with most recent video */}
+
                     {/* Add most recent 5 videos as thumbnail images to click and switch the main video above */}
                     <button
                         href="https://www.youtube.com/channel/UChuhEhCujTGP1mfmPdtuVhA"
-                        class="button_1"
+                        className="button_1"
                     >
                         Watch More
                     </button>
