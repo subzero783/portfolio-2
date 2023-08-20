26e8f20ab43c933ee34f8079e9796bf8c48882b9
“Gustavo <gusto2897@gmail.com>
2023-08-19 20:46:03 -0700

    Changing colors

--- frontend/src/components/Progress/Progress.js
+++ frontend/src/components/Progress/Progress.js
@@ -1,28 +1,26 @@
-import React from 'react'
-import styled, { keyframes } from 'styled-components'
-
+import React from "react";
+import styled, { keyframes } from "styled-components";
 
 class ProgressBar extends React.Component {
+  render() {
+    const { text } = this.props;
 
-    render() {
-        const { text } = this.props
+    const ProgressContainer = styled.div`
+      margin-bottom: 25px;
+    `;
+    const Text = styled.p`
+      font-size: 17px;
+      font-family: Poppins;
+      color: #000;
+    `;
 
-        const ProgressContainer = styled.div`
-            margin-bottom: 25px;
-        `
-        const Text = styled.p`
-            font-size: 17px;
-            font-family: Poppins;
-            color: #fff;
-        `
-
-        const Value = styled.span`
-            font-size: 17px;
-            font-family: Poppins;
-            color: #fff;
-            float: right;
-        `
-        const ColorAnimation = keyframes`
+    const Value = styled.span`
+      font-size: 17px;
+      font-family: Poppins;
+      color: #000;
+      float: right;
+    `;
+    const ColorAnimation = keyframes`
             0%  {background: #04e5e5;}
             10% {background: #f37055;}
             20% {background: #ef4e7b;}
@@ -34,25 +32,24 @@ class ProgressBar extends React.Component {
             80% {background: #5073b8;}
             90% {background: #1098ad;}
             100% {background: #f37055;}
-        `
+        `;
 
-        const Progress = styled.div`
-            height: 5px;
-            border-radius: 2.5px;
-            margin-top: 10px;
-            transition: 2s;
-            animation: ${ColorAnimation} 10s infinite alternate;
-        `
+    const Progress = styled.div`
+      height: 5px;
+      border-radius: 2.5px;
+      margin-top: 10px;
+      transition: 2s;
+      animation: ${ColorAnimation} 10s infinite alternate;
+    `;
 
-
-        return(
-            <ProgressContainer>
-                <Text>{text}</Text>
-                <Value>{this.props.value}%</Value>
-                <Progress style={{width: `${this.props.value}%`}}></Progress>
-            </ProgressContainer>
-        )
-    }
+    return (
+      <ProgressContainer>
+        <Text>{text}</Text>
+        <Value>{this.props.value}%</Value>
+        <Progress style={{ width: `${this.props.value}%` }}></Progress>
+      </ProgressContainer>
+    );
+  }
 }
 
-export default ProgressBar
+export default ProgressBar;
--- frontend/src/components/Timeline/Timeline.js
+++ frontend/src/components/Timeline/Timeline.js
@@ -1,132 +1,125 @@
-import React from 'react'
-import styled from 'styled-components'
-import htmlParse from 'html-react-parser'
+import React from "react";
+import styled from "styled-components";
+import htmlParse from "html-react-parser";
 
 class Timeline extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            selector: Object.keys(this.props.data)[0]
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      selector: Object.keys(this.props.data)[0],
+    };
+  }
 
-    render() {
-        const TimelineContainer = styled.div`
-            display: flex;
-            min-height: 250px;
-            margin-top: 60px;
-            position: relative;
-        `
+  render() {
+    const TimelineContainer = styled.div`
+      display: flex;
+      min-height: 250px;
+      margin-top: 60px;
+      position: relative;
+    `;
 
-        const TimelineSelectors = styled.div`
-            position: relative;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            bottom: 50px;
-            left: 20px;
-            &::before {
-                content: '';
-                position: absolute;
-                height: 100%;
-                width: 20%;
-                background-color: rgba(255,255,255,.5);
-                z-index: -1;
-                margin: auto;
-                left: 40%;
-                border-radius: 20px;
-            }
-            @media screen and (max-width: 767px){
-                left: 3px;
-            }
-        `
-        
-        const TimelineSelector = styled.button`
-            width: 15px;
-            height: 15px;
-            border-radius: 7.5px;
-            border: none;
-            margin: 10px 0px;
-            transition: .5s;
-            &:focus {
-                outline: none;
-            }
-            &.active, &:hover {
-                background-color: #04e5e5;
-            }
-        `
+    const TimelineSelectors = styled.div`
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      bottom: 50px;
+      left: 20px;
+      &::before {
+        content: "";
+        position: absolute;
+        height: 100%;
+        width: 20%;
+        background-color: rgba(0, 0, 0, 0.5);
+        z-index: -1;
+        margin: auto;
+        left: 40%;
+        border-radius: 20px;
+      }
+      @media screen and (max-width: 767px) {
+        left: 3px;
+      }
+    `;
 
-        const Details = styled.div`
-            display: block;
-        `
+    const TimelineSelector = styled.button`
+      width: 15px;
+      height: 15px;
+      border-radius: 7.5px;
+      border: none;
+      margin: 10px 0px;
+      transition: 0.5s;
+      background-color: #000;
+      &:focus {
+        outline: none;
+      }
+      &.active,
+      &:hover {
+        background-color: #04e5e5;
+      }
+    `;
 
-        const Heading = styled.h4`
-            font-size: 25px;
-            font-weight: bold;
-            color: #fff;
-            margin-bottom: 5px;
-        `
+    const Details = styled.div`
+      display: block;
+    `;
 
-        const Company = styled.h5`
-            font-size: 15px;
-            font-weight: 900;
-            color: #04e5e5;
-            margin-bottom: 5px;
-        `
-        const Text = styled.div`
-            font-size: 12px;
-            font-weight: 300;
-            color: #c5c5c5;
-        `
+    const Heading = styled.h4`
+      font-size: 25px;
+      font-weight: bold;
+      color: #000;
+      margin-bottom: 5px;
+    `;
 
-        const Years = styled.h5`
-            color: #fff;
-            font-weight: 900;
-            letter-spacing: 2px;
-            font-size: 80px;
-            position: absolute;
-            top: -60px;
-            opacity: 0.2;
-            @media (max-width:1439px) {
-                font-size: 40px;
-                top: -38px;
-            }
-            @media (max-width:767px) {
-                font-size: 34px;
-                top: -35px;
-                opacity: 0.2;
-            }
-            `
+    const Company = styled.h5`
+      font-size: 15px;
+      font-weight: 900;
+      color: #000;
+      margin-bottom: 5px;
+    `;
+    const Text = styled.div`
+      font-size: 12px;
+      font-weight: 300;
+      color: #000;
+    `;
 
+    const Years = styled.h5`
+      color: #000;
+      font-weight: 900;
+      letter-spacing: 2px;
+      font-size: 80px;
+      position: absolute;
+      top: -60px;
+      opacity: 0.2;
+      @media (max-width: 1439px) {
+        font-size: 40px;
+        top: -38px;
+      }
+      @media (max-width: 767px) {
+        font-size: 34px;
+        top: -35px;
+        opacity: 0.2;
+      }
+    `;
 
-        const selector = this.state.selector
-        let { data } = this.props
+    const selector = this.state.selector;
+    let { data } = this.props;
 
-        return(
-            <TimelineContainer>
-                <Details>
-                    <Years>{data[selector].date}</Years>
-                    <Heading>{data[selector].title}</Heading>
-                    <Company>{data[selector].institution}</Company>
-                    <Text>
-                        {htmlParse(data[selector].description)}
-                    </Text>
-                </Details>
-                <TimelineSelectors>
-                    {
-                   
-                    Object.keys(this.props.data).map((value, index) => (
-                        <TimelineSelector
-                            key={index}
-                            onClick={() => this.setState({selector : value})}
-                            className={this.state.selector === value ? "active" : ""}
-                        />
-                    ))}
-                </TimelineSelectors>
-            </TimelineContainer>
-        )
-    }
+    return (
+      <TimelineContainer>
+        <Details>
+          <Years>{data[selector].date}</Years>
+          <Heading>{data[selector].title}</Heading>
+          <Company>{data[selector].institution}</Company>
+          <Text>{htmlParse(data[selector].description)}</Text>
+        </Details>
+        <TimelineSelectors>
+          {Object.keys(this.props.data).map((value, index) => (
+            <TimelineSelector key={index} onClick={() => this.setState({ selector: value })} className={this.state.selector === value ? "active" : ""} />
+          ))}
+        </TimelineSelectors>
+      </TimelineContainer>
+    );
+  }
 }
 
-export default Timeline
+export default Timeline;
